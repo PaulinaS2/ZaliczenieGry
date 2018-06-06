681acbb8a5e26191ea8ca92c3d0cfb10eea7d046
Paulina <paulinasmolka@fastmail.com>
2018-06-06 14:53:36 +0200

    Bez zmian.

---
+++ src/kolkoikrzyzyk/Element.java
@@ -0,0 +1,22 @@
+package kolkoikrzyzyk;
+
+import javax.swing.JButton;
+import java.awt.Graphics;
+
+public class Element extends JButton{
+
+    int stan=0;
+    public Element(){}
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        if(stan == 1){
+            g.drawLine(0, 0, 160, 150);
+            g.drawLine(130, 0, 0, 120);
+        }
+        else if(stan == 2){
+            g.drawOval(10, 10, 110, 110);
+        }
+    }
+}
---
+++ src/kolkoikrzyzyk/KolkoIKrzyzyk.java
@@ -0,0 +1,35 @@
+package kolkoikrzyzyk;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class KolkoIKrzyzyk extends JFrame{
+    public static void main(String[] args){
+        KolkoIKrzyzyk kik = new KolkoIKrzyzyk();
+        kik.setMinimumSize(new Dimension(400, 400));
+        kik.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        kik.setResizable(false);
+        kik.setTitle("Gra w Kółko i Krzyzyk");
+        JPanel plansza = new JPanel();
+        plansza.setLayout(new GridLayout(3,3));
+        Element a[][]= new Element[3][3];
+        int wyniki[][]= new int[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                a[i][j]= new Element();
+                wyniki[i][j]=0;
+            }
+        }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                a[i][j].addActionListener(new ElementsListener(a, wyniki, plansza));
+                plansza.add(a[i][j]);
+            }
+        }
+
+        kik.add(plansza);
+        kik.setVisible(true);
+    }
+}
